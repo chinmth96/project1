29986cda1e19d33409ebd4001dd226f710415fff
chinmth96 <chinmth1901006@fpt.edu.vn>
2019-05-20 14:28:25 +0700

    hello

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extened:false}))
+app.set('view engine', 'ejs');
+
+app.get('/register',(req,res)=> res.render('register'))
+
+app.post('/welcome', (req,res) => res.send('hello' + req.body.username))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
