f376c23e850af9d08e791e8170c25dd37d66b4ab
Giang <giangvhtth1901009@fpt.edu.vn>
2019-05-20 16:20:42 +0700

    Test comment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extened:false}))
 app.set('view engine', 'ejs');
-
+/* Test comment */
 app.get('/register',(req,res)=> res.render('register'))
 
 app.post('/welcome', (req,res) => res.send('hello' + req.body.username))
